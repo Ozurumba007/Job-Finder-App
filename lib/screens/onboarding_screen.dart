59e38225b2e898f32355faed855327e9ddf275e5
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-10-08 11:20:11 +0100

    added a button to the onboarding screen

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -14,7 +14,6 @@ class OnboardingScreen extends StatelessWidget {
           Expanded(
             child: Container(
               width: double.infinity,
-              height: 300,
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/recruiter.jpg'),
@@ -23,40 +22,64 @@ class OnboardingScreen extends StatelessWidget {
               ),
             ),
           ),
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-              child: Container(
-                color: Colors.white,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      // SizedBox(height: 20),
-                      // title of the onbarding screen
-                      Text(
-                        'Shape Your Future\nWith Job Opportunities',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
+          Container(
+            height: 250,
+            child: Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                child: Container(
+                  color: Colors.white,
+                  height: 500,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // SizedBox(height: 20),
+                        // title of the onbarding screen
+                        Text(
+                          'Shape Your Future\nWith Job Opportunities',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      // SizedBox(height: 20),
-                      // subTitle of the onbarding screen
-                      Text(
-                        'Discover a vast selection of over 30,000\njob opportunities to supercharge your\ncareer path today.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey[500],
+                        // SizedBox(height: 20),
+                        // subTitle of the onbarding screen
+                        Text(
+                          'Discover a vast selection of over 30,000\njob opportunities to supercharge your\ncareer path today.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[500],
+                          ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Container(
+                            height: 45,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.blue.shade300),
+                            child: Center(
+                              child: Text(
+                                'Get Started',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
