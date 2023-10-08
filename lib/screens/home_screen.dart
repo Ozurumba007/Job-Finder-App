8eb8896584d6db513145bc57afa08179b20558ec
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-10-08 12:25:46 +0100

    added a headline text \ added a textfield widget to the home_screen \ added a prefix icon and a suffix icon \ added a hint text to the textfield

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,17 +11,53 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       // backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 10),
 
               // Custom app bar
               CustomAppBar(),
+
+              SizedBox(height: 20),
+              // Headline Text
+              Text(
+                'Find a job you are\ninterested in',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.grey[200],
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.filter_list,
+                      color: Colors.black,
+                    ),
+                    hintText: 'Search jobs, company, city ',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
