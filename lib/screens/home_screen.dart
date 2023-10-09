19fdcea44d7710437e291ae85a4e8fd1fc74ee27
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-10-09 10:07:16 +0100

    created a file called find a job container in the component folder \ the find a job container is mainly for reducing the amount of code in the home_screen.dart file and makes  the home_screen code more readable and more understandable

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:job_search_app/component/custom_app_bar.dart';
 
 import '../component/custom_textfield.dart';
+import '../component/find_a_job_container.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,22 +14,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       // backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 10),
+              SizedBox(height: 10),
 
               // Custom app bar
-              const CustomAppBar(),
+              CustomAppBar(),
 
-              const SizedBox(height: 20),
+              SizedBox(height: 20),
               // Headline Text
-              const Text(
+              Text(
                 'Find a job you are\ninterested in',
                 style: TextStyle(
                   fontSize: 30,
@@ -38,68 +39,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
               // Textfield widget
 
-              const SizedBox(height: 20),
-              const CustomTextField(),
+              SizedBox(height: 20),
+              CustomTextField(),
 
               // Container (Lets find a job for you + button +image),
 
-              const SizedBox(height: 20),
-              Container(
-                height: 150,
-                width: double.infinity,
-                // padding: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.blue.shade200,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Let\'s find a job that\nsuits you',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            height: 50,
-                            width: 150,
-                            child: Center(
-                              child: Text(
-                                'Find Now',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: 145,
-                      width: 150,
-                      // color: Colors.grey,
-                      child: Image.asset(
-                        'assets/woman2.png',
-                        fit: BoxFit.cover,
-                      ),
-                    )
-                  ],
-                ),
-              ),
+              SizedBox(height: 20),
+
+              FindAJobContainer(),
             ],
           ),
         ),
