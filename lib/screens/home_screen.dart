993ee011bae4a0af8a945a4363e3beb70f06468a
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-10-09 10:01:19 +0100

    created the let's find a job that suits you container \ added a button in the container \ added an image in the container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -45,11 +45,59 @@ class _HomeScreenState extends State<HomeScreen> {
 
               const SizedBox(height: 20),
               Container(
-                height: 120,
+                height: 150,
                 width: double.infinity,
+                // padding: EdgeInsets.all(15),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(12),
                   color: Colors.blue.shade200,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Let\'s find a job that\nsuits you',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            height: 50,
+                            width: 150,
+                            child: Center(
+                              child: Text(
+                                'Find Now',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 145,
+                      width: 150,
+                      // color: Colors.grey,
+                      child: Image.asset(
+                        'assets/woman2.png',
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
